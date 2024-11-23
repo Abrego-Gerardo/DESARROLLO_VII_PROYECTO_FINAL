51708094fd44f0cf3de68f54ec6526b32c6e80dc
Abrego-Gerardo <143464224+Abrego-Gerardo@users.noreply.github.com>
2024-11-23 18:23:10 -0500

    Update BD_Agencia.sql

--- database/BD_Agencia.sql
+++ database/BD_Agencia.sql
@@ -37,3 +37,73 @@ INSERT INTO `destinos` (`id`, `tipo_destino`, `pais`, `city`, `precio_nino`, `pr
 ALTER TABLE `destinos` ADD `detalles` TEXT DEFAULT NULL;
 ALTER TABLE `destinos` MODIFY `id` INT(10) NOT NULL AUTO_INCREMENT;
 ALTER TABLE `destinos` ADD PRIMARY KEY (`id`);
+
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 23-11-2024 a las 18:15:22
+-- Versión del servidor: 10.4.24-MariaDB
+-- Versión de PHP: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `agencia_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(80) NOT NULL,
+  `email` varchar(80) NOT NULL,
+  `password` varchar(80) NOT NULL,
+  `usertype` varchar(50) NOT NULL DEFAULT 'usuario'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `email`, `password`, `usertype`) VALUES
+(1, 'Kevin1', 'delgadokev13@gmail.com', '$2y$10$qkD7nk89XnYY9N12phvQxOFtUZQte6Cnjq328Zj0K8zrnOIcCLx2q', 'usuario'),
+(2, 'KevinAdmin', 'kevin.delgado1@utp.ac.pa', '$2y$10$e6n.8kjJt4g/u31XJUCqO.ng.Ygq4hR0H4KxDWrJ8LaWMOkN3Ed9S', 'admin');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
